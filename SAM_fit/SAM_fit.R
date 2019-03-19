72829298e8b79f8167aa8ca9534a35847c3f1131
jchampag <juliette.champagnat@ifremer.fr>
2019-03-19 18:07:57 -0230

    SAM trials
    - 3Ps.RData are from Matt last upload
    - 1 R script to set up data + 2 RData file ready to be use in SAM (IO/OFF index)
    - 1 R script for SAM fitting
    - saving all output+configuration in a folder

---
+++ SAM_fit/SAM_fit.R
@@ -0,0 +1,179 @@
+# SAM AT https://github.com/fishfollower/SAM 
+#install.packages("Rcpp")
+#devtools::install_github("fishfollower/SAM/stockassessment")
+
+#install.packages("corplot")
+#library(corplot)
+rm(list=ls())
+library(TMB);library(ggplot2);library(gridExtra);library(plyr);library(tidyverse);library(reshape2); library(dplyr);library(ggplot2)
+library(lattice);library(stats4);library(MASS);library(car);library("xtable") 
+library(stockassessment)
+
+#load data
+load("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_IO.Rdata")
+#load("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_OFF.RData")
+
+dat <- setup.sam.data(
+  surveys=surveys,
+  residual.fleet=cn, 
+  prop.mature=mo, 
+  stock.mean.weight=sw, 
+  catch.mean.weight=cw, 
+  dis.mean.weight=dw, 
+  land.mean.weight=lw,
+  prop.f=pf, 
+  prop.m=pm, 
+  natural.mortality=nm, 
+  land.frac=lf)
+
+#class(cn);class(mo);class(sw);class(cw);class(dw);
+#class(lf);class(pf);class(pm);class(lw);class(nm);
+#class(surveys);
+#str(surveys)
+
+
+##########
+# Set up configuration
+###########
+conf <- defcon(dat)
+
+#should be 2
+conf$minAge
+
+#should be 14
+conf$maxAge
+
+#last age is a plus group so should 1 (no plus group=0)
+conf$maxAgePlusGroup
+
+#coupling fishing mortality states, try varying this, if make them all indep then appears to overfit the catch
+conf$keyLogFsta
+#na <- conf$maxAge-conf$minAge+1 
+#conf$keyLogFsta[1,]=rep(0,na) #same for all age
+
+# Correlation of fishing mortality across ages (0 independent, 1 compound symmetry, or 2 AR(1)
+conf$corFlag 
+
+# Coupling of the survey catchability parameters (nomally first row is not used, as that is covered by fishing mortality).  
+conf$keyLogFpar 
+
+#density dep catchability parms
+conf$keyQpow
+
+# Coupling of process variance parameters for log(F)-process (nomally only first row is used)
+conf$keyVarF
+
+# Coupling of process variance parameters for log(N)-process
+conf$keyVarLogN
+
+#coupling of var parms for the obs
+conf$keyVarObs
+
+# Covariance structure for each fleet ("ID" independent, "AR" AR(1), or "US" for unstructured). | Possible values are: "ID" "AR" "US"
+conf$obsCorStruct
+
+conf$fbarRange <- c(4,6)
+
+#######
+##Fit
+#######
+par <- defpar(dat,conf)
+fit <- sam.fit(dat,conf,par)
+
+saveConf (conf , file ="model.cfg") 
+save(fit,file = 'SAM_fit.RData')
+
+#######
+##ouptput et plot
+#######
+setwd('C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit')
+fit1 <-fit
+
+#global plot: SSB,R Fbar
+#windows();plot(fit1)
+jpeg(file='global_fit.jpeg',width=5,height=8,units='in',res=300) ;plot(fit1);dev.off()
+
+##correlation between observation
+#windows();corplot(fit1) #idem obscorrplot(fit1)
+jpeg(file='correlation_fleet.jpeg',width=8,height=8,units='in',res=300) ;corplot(fit1);dev.off()
+
+#one step ahead residual
+#res1 <-residuals(fit1)
+#windows();plot(res1)
+jpeg(file='step.ahead.residual.jpeg',width=8,height=8,units='in',res=300) ;plot(res1);dev.off()
+
+#join sample residual
+resp <- procres(fit)
+#windows();plot(resp)
+jpeg(file='resample.residual.jpeg',width=8,height=8,units='in',res=300) ;plot(res1);dev.off()
+
+#catch plot
+#windows();catchplot(fit1)
+jpeg(file='catch_plot.jpeg',width=8,height=8,units='in',res=300) ;catchplot(fit1);dev.off()
+
+#fleet fit to observation
+#windows();fitplot(fit1)
+jpeg(file='fit_fleet.jpeg',width=8,height=5,units='in',res=300) ;fitplot(fit1);dev.off()
+attributes(surveys)
+jpeg(file='fit_fleet_catch.jpeg',width=8,height=5,units='in',res=300) ;fitplot(fit1,fleets=1);dev.off()
+jpeg(file='fit_fleet_GEAC.jpeg',width=6,height=5,units='in',res=300) ;fitplot(fit1,fleets = 2);dev.off()
+jpeg(file='fit_fleet_ERHAPS.jpeg',width=6,height=5,units='in',res=300) ;fitplot(fit1,fleets = 3);dev.off()
+jpeg(file='fit_fleet_OFF.jpeg',width=6,height=5,units='in',res=300) ;fitplot(fit1,fleets = 4);dev.off()
+
+
+##Fishing mortality
+#windows();fbarplot(fit1)
+jpeg(file='Fbar.jpeg',width=6,height=5,units='in',res=300) ;fbarplot(fit1);dev.off()
+F1 <-as.data.frame(faytable(fit1))
+F1$years <- as.numeric(rownames(F1))
+F1_l <- melt(F1,id.vars='years',value.name = "F",variable.name = "age")
+#windows();wireframe(F ~ years*age, data = F1_l,zoom=0.95,xlab = "Year", ylab = "Age",main = "F", drape = TRUE,colorkey = TRUE,screen = list(z = -60, x = -60))
+jpeg(file='3D_F.jpeg',width=6,height=5,units='in',res=300) ;wireframe(F ~ years*age, data = F1_l,zoom=0.95,xlab = "Year", ylab = "Age",
+                                    main = "F",drape = TRUE,colorkey = TRUE,screen = list(z = -60, x = -60));dev.off()
+#windows();ggplot(F1_l)+geom_line(aes(years,F,col=age))
+jpeg(file='F.jpeg',width=6,height=5,units='in',res=300) ;ggplot(F1_l)+geom_line(aes(years,F,col=age));dev.off()
+
+
+
+####retrospective plot
+ny.retro <- 5
+#retro1 <- retro(fit1,ny.retro)
+#windows();plot(retro1)
+jpeg(file='retro_plot.jpeg',width=6,height=8,units='in',res=300) ;plot(retro1);dev.off()
+
+##leave out fleet plot
+#lo <- leaveout(fit1)
+#windows();plot(lo)
+jpeg(file='leaveout_plot.jpeg',width=6,height=8,units='in',res=300) ;plot(lo);dev.off()
+
+#Quatchabilities
+#windows();qtableplot(qtable(fit1))
+jpeg(file='Q.jpeg',width=6,height=8,units='in',res=300) ;qtableplot(Q);dev.off()
+
+#parameter plot => bof
+#windows();parplot(fit1)
+
+#didn't those function to work
+#window();ssbplot(fit1)
+#window();recplot(fit1)
+#coef.sam(fit1)
+
+##save stuff
+#coefficient
+sd.rep <-sdreport(fit$obj)
+
+write.table(summary(fit,digits=5),file='output.dat') 
+write.table( modeltable(fit1),file='modelSummary.dat')
+write.table( cbind(sd.rep$value,sd.rep$sd),file='raw_output.txt')
+
+
+
+#######
+#Model comparison
+#######
+#load different fit
+fits <- c(old=fit1 , new = fit2 )
+fits <- c(fit1,fit2,fit3 )
+modeltable ( fits )
+windows();plot(fits , addCI = TRUE )
+
---
+++ SAM_fit/SAMdata_setup.R
@@ -0,0 +1,98 @@
+# SAM AT https://github.com/fishfollower/SAM 
+#install.packages("Rcpp")
+#devtools::install_github("fishfollower/SAM/stockassessment")
+
+rm(list=ls())
+library(TMB);library(plyr);library(tidyverse);library(reshape2); library(dplyr)
+library(stockassessment)
+
+#load data => newest dataset from brach:SSM/SepF_std+comm_wts+rhoq+all_survs
+load("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/3PS.Rdata")
+#load("C:/Users/jchampag/Documents/GitHub/SSM3PS/data_EDA/3PS.Rdata")
+
+###DATA SET UP IN SAM FORMAT
+
+#stock weight
+names(wt) <- substr(names(wt),4,nchar(names(wt)))
+rownames(wt) <- assess.year
+sw <- as.matrix(wt)
+
+#catch number
+names(catch) <- substr(names(catch),4,nchar(names(catch)))
+rownames(catch) <- assess.year
+cn <- as.matrix(catch)
+
+#catch weight /!\ age 2 is missing
+names(comm_wt) <- substr(names(comm_wt),4,nchar(names(comm_wt)))
+rownames(comm_wt) <- assess.year
+comm_wt$'2' <-sw[,1] #using sw for age2 for now, Jon extrapolation migth be usefull here
+cw <- as.matrix(comm_wt[,c(14,2:13)])
+
+#landing and discard table
+dw <- cw #same than catch weight
+lf <- cn;lf[] <-1 #landing fraction = 100%
+lw <- cw #same than catch weight
+
+#maturity
+names(mat) <- substr(names(mat),4,nchar(names(mat)))
+rownames(mat) <- assess.year
+mo <- as.matrix(mat)
+
+#natural mortality
+nm <- cn;nm[] <-0.2
+
+#proportion of mature and dead for ssb calculation = 1st Jan so 0
+pf <-  cn;pf[] <-0
+pm <-  cn;pm[] <-0
+
+
+
+#surveys => annoying process, made it on excel and import it througth :read.ices
+# ?/!\ two files can be used surveys-FR-GEAC-IO.txt OR surveys-FR-GEAC-OFF.txt depending what you want
+
+#unique(indices$survey)
+#french <- as.data.frame(dcast(indices[indices$survey == "FRANCE",],Year~Age,value.var = 'index'))
+#rownames(french) <- french$Year
+#geac <- as.data.frame(dcast(indices[indices$survey == "GEAC",],Year~Age,value.var = 'index'))
+#rownames(geac) <- geac$Year
+#both <- as.data.frame(dcast(indices[indices$survey == "BOTH",],Year~Age,value.var = 'index')) ##not working, probably more than 1 survey
+#RV_IO <- as.data.frame(RV.IO.matrix)
+#rownames(RV_IO) <- substr(as.character(RV_IO$Year),1,4)
+#names(RV_IO) <- substr(names(RV_IO),4,nchar(names(RV_IO)))
+#RV_OFF <- as.data.frame(RV.OFF.matrix)
+#RV_OFF <- RV_OFF[RV_OFF$Year != '1993.3',] #two survey in 1993, choose to keep 1993.1 or 1993.3
+#rownames(RV_OFF) <-  substr(as.character(RV_OFF$Year),1,4)
+#names(RV_OFF) <- substr(names(RV_OFF),4,nchar(names(RV_OFF)))
+#surveys1 <- list(french=french[,-1],geac=geac[,-1],off=RV_OFF[,-1])#or io=RV_IO[,-1]  )
+#write.csv(geac,'C:/Users/jchampag/Dropbox/IFREMER/3Ps_cod/Cours/Cours_Noel_Cadigan/F6005_winter_2019/Project1/GEAC.csv')
+
+#choose the survey indices you wanna use
+surveys <- read.ices("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-IO.txt");survey <- 'IO'
+surveys <- read.ices("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-OFF.txt");survey <- 'OFF'
+#surveys2 <- read.ices("C:/Users/ChampagnatJ/Dropbox/IFREMER/3Ps_cod/Cours/Cours_Noel_Cadigan/F6005_winter_2019/Project1/surveys-FR-GEAC-OFF.txt")
+
+##check consistency between all tables
+dim(cn);dim(cw);dim(dw);dim(lf);dim(lw);dim(mo);dim(nm);dim(pf);dim(pm);dim(sw);dim(surveys)
+dim(surveys$`GEAC													`);dim(surveys$`IO													`);dim(surveys$`ERHAPS													`);dim(surveys2$`OFF													`)
+
+rownames(cn);rownames(cw);rownames(dw);rownames(lf);rownames(lw);
+rownames(mo);rownames(nm);rownames(pf);rownames(pm);rownames(sw)
+
+#check if data are accecpted by SAM data set up
+dat <- setup.sam.data(
+  surveys=surveys,
+  residual.fleet=cn, 
+  prop.mature=mo, 
+  stock.mean.weight=sw, 
+  catch.mean.weight=cw, 
+  dis.mean.weight=dw, 
+  land.mean.weight=lw,
+  prop.f=pf, 
+  prop.m=pm, 
+  natural.mortality=nm, 
+  land.frac=lf)
+
+
+#saving in a Rdata file
+save(surveys,cn,mo,sw,cw,dw,lw,pf,pm,nm,lf,file=paste("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_",survey,".Rdata",sep=""))
+save(surveys,cn,mo,sw,cw,dw,lw,pf,pm,nm,lf,file=paste("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_",survey,".Rdata",sep=""))
