3a4cf69e604874ad60e81812b97b664ff690f91b
jchampag <juliette.champagnat@ifremer.fr>
2019-03-20 17:09:36 -0230

    update with IO index from Matt

--- SAM_fit/SAMdata_setup.R
+++ SAM_fit/SAMdata_setup.R
@@ -25,7 +25,7 @@ cn <- as.matrix(catch)
 #catch weight /!\ age 2 is missing
 names(comm_wt) <- substr(names(comm_wt),4,nchar(names(comm_wt)))
 rownames(comm_wt) <- assess.year
-comm_wt$'2' <-sw[,1] #using sw for age2 for now, Jon extrapolation migth be usefull here
+comm_wt$'2' <-cn[,1]*sw[,1] #using cn*sw for age2 for now, Jon extrapolation migth be usefull here
 cw <- as.matrix(comm_wt[,c(14,2:13)])
 
 #landing and discard table
@@ -55,7 +55,7 @@ pm <-  cn;pm[] <-0
 #rownames(french) <- french$Year
 #geac <- as.data.frame(dcast(indices[indices$survey == "GEAC",],Year~Age,value.var = 'index'))
 #rownames(geac) <- geac$Year
-#both <- as.data.frame(dcast(indices[indices$survey == "BOTH",],Year~Age,value.var = 'index')) ##not working, probably more than 1 survey
+#both <- as.data.frame(dcast(indices[indices$survey == "BOTH",],Year~Age,value.var = 'index',fun=mean)) ##not working, probably more than 1 survey
 #RV_IO <- as.data.frame(RV.IO.matrix)
 #rownames(RV_IO) <- substr(as.character(RV_IO$Year),1,4)
 #names(RV_IO) <- substr(names(RV_IO),4,nchar(names(RV_IO)))
@@ -64,16 +64,16 @@ pm <-  cn;pm[] <-0
 #rownames(RV_OFF) <-  substr(as.character(RV_OFF$Year),1,4)
 #names(RV_OFF) <- substr(names(RV_OFF),4,nchar(names(RV_OFF)))
 #surveys1 <- list(french=french[,-1],geac=geac[,-1],off=RV_OFF[,-1])#or io=RV_IO[,-1]  )
-#write.csv(geac,'C:/Users/jchampag/Dropbox/IFREMER/3Ps_cod/Cours/Cours_Noel_Cadigan/F6005_winter_2019/Project1/GEAC.csv')
+#write.csv(both,'C:/Users/jchampag/Dropbox/IFREMER/3Ps_cod/Cours/Cours_Noel_Cadigan/F6005_winter_2019/Project1/both.csv')
 
 #choose the survey indices you wanna use
-surveys <- read.ices("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-IO.txt");survey <- 'IO'
+surveys <- read.ices("C:/Users/jchampag/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-IO.txt");survey <- 'IO'
 surveys <- read.ices("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-OFF.txt");survey <- 'OFF'
 #surveys2 <- read.ices("C:/Users/ChampagnatJ/Dropbox/IFREMER/3Ps_cod/Cours/Cours_Noel_Cadigan/F6005_winter_2019/Project1/surveys-FR-GEAC-OFF.txt")
 
 ##check consistency between all tables
 dim(cn);dim(cw);dim(dw);dim(lf);dim(lw);dim(mo);dim(nm);dim(pf);dim(pm);dim(sw);dim(surveys)
-dim(surveys$`GEAC													`);dim(surveys$`IO													`);dim(surveys$`ERHAPS													`);dim(surveys2$`OFF													`)
+dim(surveys$`GEAC													`);dim(surveys$`IO													`);dim(surveys$`ERHAPS													`);dim(surveys$`OFF													`)
 
 rownames(cn);rownames(cw);rownames(dw);rownames(lf);rownames(lw);
 rownames(mo);rownames(nm);rownames(pf);rownames(pm);rownames(sw)
@@ -94,5 +94,4 @@ dat <- setup.sam.data(
 
 
 #saving in a Rdata file
-save(surveys,cn,mo,sw,cw,dw,lw,pf,pm,nm,lf,file=paste("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_",survey,".Rdata",sep=""))
-save(surveys,cn,mo,sw,cw,dw,lw,pf,pm,nm,lf,file=paste("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_",survey,".Rdata",sep=""))
+save(surveys,cn,mo,sw,cw,dw,lw,pf,pm,nm,lf,file=paste("C:/Users/jchampag/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_",survey,".Rdata",sep=""))
