c92175361c25183847b7fb17655b6baf067434dc
jchampag <juliette.champagnat@ifremer.fr>
2019-03-22 15:04:18 -0230

    trying few run

--- SAM_fit/SAM_fit.R
+++ SAM_fit/SAM_fit.R
@@ -13,18 +13,19 @@ library(stockassessment)
 load("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_IO.Rdata")
 #load("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_OFF.RData")
 
+
 dat <- setup.sam.data(
   surveys=surveys,
-  residual.fleet=cn, 
-  prop.mature=mo, 
-  stock.mean.weight=sw, 
-  catch.mean.weight=cw, 
-  dis.mean.weight=dw, 
-  land.mean.weight=lw,
-  prop.f=pf, 
-  prop.m=pm, 
-  natural.mortality=nm, 
-  land.frac=lf)
+  residual.fleet=cn[c(4:58),], 
+  prop.mature=mo[c(4:58),], 
+  stock.mean.weight=sw[c(4:58),], 
+  catch.mean.weight=cw[c(4:58),], 
+  dis.mean.weight=dw[c(4:58),], 
+  land.mean.weight=lw[c(4:58),],
+  prop.f=pf[c(4:58),], 
+  prop.m=pm[c(4:58),], 
+  natural.mortality=nm[c(4:58),], 
+  land.frac=lf[c(4:58),])
 
 #class(cn);class(mo);class(sw);class(cw);class(dw);
 #class(lf);class(pf);class(pm);class(lw);class(nm);
@@ -48,6 +49,7 @@ conf$maxAgePlusGroup
 
 #coupling fishing mortality states, try varying this, if make them all indep then appears to overfit the catch
 conf$keyLogFsta
+conf$keyLogFsta[1,] <- c(1,1,2,3,4,4,4,4,5,6,7,8,9)
 #na <- conf$maxAge-conf$minAge+1 
 #conf$keyLogFsta[1,]=rep(0,na) #same for all age
 
@@ -59,6 +61,11 @@ conf$keyLogFpar
 
 #density dep catchability parms
 conf$keyQpow
+conf$keyQpow[2,] <- c(1,1,2,3,4,4,4,4,4,4,4,4,4)
+conf$keyQpow[3,] <- c(11,11,12,13,14,14,14,14,14,14,14,14,14)
+conf$keyQpow[4,] <- c(21,21,22,23,24,24,24,24,24,24,24,24,24)
+
+
 
 # Coupling of process variance parameters for log(F)-process (nomally only first row is used)
 conf$keyVarF
@@ -72,7 +79,7 @@ conf$keyVarObs
 # Covariance structure for each fleet ("ID" independent, "AR" AR(1), or "US" for unstructured). | Possible values are: "ID" "AR" "US"
 conf$obsCorStruct
 
-conf$fbarRange <- c(4,6)
+conf$fbarRange <- c(4,9)
 
 #######
 ##Fit
@@ -98,14 +105,14 @@ jpeg(file='global_fit.jpeg',width=5,height=8,units='in',res=300) ;plot(fit1);dev
 jpeg(file='correlation_fleet.jpeg',width=8,height=8,units='in',res=300) ;corplot(fit1);dev.off()
 
 #one step ahead residual
-#res1 <-residuals(fit1)
+res1 <-residuals(fit1)
 #windows();plot(res1)
-jpeg(file='step.ahead.residual.jpeg',width=8,height=8,units='in',res=300) ;plot(res1);dev.off()
+jpeg(file='residual.step.ahead.jpeg',width=8,height=8,units='in',res=300) ;plot(res1);dev.off()
 
 #join sample residual
 resp <- procres(fit)
 #windows();plot(resp)
-jpeg(file='resample.residual.jpeg',width=8,height=8,units='in',res=300) ;plot(res1);dev.off()
+jpeg(file='residual.resample.jpeg',width=8,height=8,units='in',res=300) ;plot(res1);dev.off()
 
 #catch plot
 #windows();catchplot(fit1)
@@ -118,7 +125,7 @@ attributes(surveys)
 jpeg(file='fit_fleet_catch.jpeg',width=8,height=5,units='in',res=300) ;fitplot(fit1,fleets=1);dev.off()
 jpeg(file='fit_fleet_GEAC.jpeg',width=6,height=5,units='in',res=300) ;fitplot(fit1,fleets = 2);dev.off()
 jpeg(file='fit_fleet_ERHAPS.jpeg',width=6,height=5,units='in',res=300) ;fitplot(fit1,fleets = 3);dev.off()
-jpeg(file='fit_fleet_OFF.jpeg',width=6,height=5,units='in',res=300) ;fitplot(fit1,fleets = 4);dev.off()
+jpeg(file='fit_fleet_IO.jpeg',width=6,height=5,units='in',res=300) ;fitplot(fit1,fleets = 4);dev.off()
 
 
 ##Fishing mortality
@@ -128,33 +135,45 @@ F1 <-as.data.frame(faytable(fit1))
 F1$years <- as.numeric(rownames(F1))
 F1_l <- melt(F1,id.vars='years',value.name = "F",variable.name = "age")
 #windows();wireframe(F ~ years*age, data = F1_l,zoom=0.95,xlab = "Year", ylab = "Age",main = "F", drape = TRUE,colorkey = TRUE,screen = list(z = -60, x = -60))
-jpeg(file='3D_F.jpeg',width=6,height=5,units='in',res=300) ;wireframe(F ~ years*age, data = F1_l,zoom=0.95,xlab = "Year", ylab = "Age",
+jpeg(file='F_3D1.jpeg',width=6,height=5,units='in',res=300) ;wireframe(F ~ years*age, data = F1_l,zoom=0.95,xlab = "Year", ylab = "Age",
                                     main = "F",drape = TRUE,colorkey = TRUE,screen = list(z = -60, x = -60));dev.off()
 #windows();ggplot(F1_l)+geom_line(aes(years,F,col=age))
 jpeg(file='F.jpeg',width=6,height=5,units='in',res=300) ;ggplot(F1_l)+geom_line(aes(years,F,col=age));dev.off()
+jpeg(file='F_3D2.jpeg',width=7,height=7,units='in',res=300) ;#windows();
+persp(x=F1$years,y=c(2:14),z=as.matrix(F1[,c(-14)]),main="F-at-age Surface",xlab='Year',ylab='Age',zlab='F',col="lightgreen",theta=130, phi=40,
+      r=50,  d=0.1, expand=0.5, ltheta=90, lphi=180,shade=0.25, ticktype="detailed", nticks=5) ;dev.off()
 
 
 
 ####retrospective plot
 ny.retro <- 5
-#retro1 <- retro(fit1,ny.retro)
+retro1 <- retro(fit1,ny.retro)
 #windows();plot(retro1)
 jpeg(file='retro_plot.jpeg',width=6,height=8,units='in',res=300) ;plot(retro1);dev.off()
 
 ##leave out fleet plot
-#lo <- leaveout(fit1)
+lo <- leaveout(fit1)
 #windows();plot(lo)
 jpeg(file='leaveout_plot.jpeg',width=6,height=8,units='in',res=300) ;plot(lo);dev.off()
 
 #Quatchabilities
 #windows();qtableplot(qtable(fit1))
-jpeg(file='Q.jpeg',width=6,height=8,units='in',res=300) ;qtableplot(Q);dev.off()
+jpeg(file='Q.jpeg',width=6,height=8,units='in',res=300) ;qtableplot(qtable(fit1));dev.off()
+
+##SSB plot
+#window();ssbplot(fit1)
+SSB <-as.data.frame(ssbtable(fit1))
+SSB$year <- as.numeric(rownames(SSB))
+SSB$Est_rel <- SSB$Estimate/SSB[SSB$year==1994,]$Estimate
+SSB$up_rel <- SSB$High/SSB[rownames(SSB)==1994,]$High
+SSB$low_rel <- SSB$Low/SSB[rownames(SSB)==1994,]$Low
+windows();ggplot(SSB)+geom_line(aes(year,Est_rel))+geom_ribbon(aes(year,ymin=low_rel,ymax=up_rel),alpha=0.5)
 
 #parameter plot => bof
 #windows();parplot(fit1)
 
 #didn't those function to work
-#window();ssbplot(fit1)
+
 #window();recplot(fit1)
 #coef.sam(fit1)
 
--- SAM_fit/SAMdata_setup.R
+++ SAM_fit/SAMdata_setup.R
@@ -10,6 +10,13 @@ library(stockassessment)
 load("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/3PS.Rdata")
 #load("C:/Users/jchampag/Documents/GitHub/SSM3PS/data_EDA/3PS.Rdata")
 
+##plot of weight
+windows();ggplot()+geom_line(data=wt.vec,aes(Year,index),col=1)+geom_line(data=midy_wt.vec,aes(Year,index),col=2)+
+  geom_line(data=comm_wt.vec,aes(Year,index),col=3)+facet_wrap(~Age,scale='free')
+windows();ggplot()+geom_line(data=catch.vec,aes(Year,index),col=1)+facet_wrap(~Age,scale='free')+
+  geom_line(data=indices,aes(Year,index,col=survey))
+windows();ggplot()+geom_line(data=catch.vec,aes(Year,index,col=factor(Age)))#+facet_wrap(~Age,scale='free')
+
 ###DATA SET UP IN SAM FORMAT
 
 #stock weight
@@ -25,7 +32,7 @@ cn <- as.matrix(catch)
 #catch weight /!\ age 2 is missing
 names(comm_wt) <- substr(names(comm_wt),4,nchar(names(comm_wt)))
 rownames(comm_wt) <- assess.year
-comm_wt$'2' <-cn[,1]*sw[,1] #using cn*sw for age2 for now, Jon extrapolation migth be usefull here
+comm_wt$'2' <-midy_wt[,1] #using midy_sw for age2 for now, Jon extrapolation migth be usefull here
 cw <- as.matrix(comm_wt[,c(14,2:13)])
 
 #landing and discard table
@@ -67,7 +74,7 @@ pm <-  cn;pm[] <-0
 #write.csv(both,'C:/Users/jchampag/Dropbox/IFREMER/3Ps_cod/Cours/Cours_Noel_Cadigan/F6005_winter_2019/Project1/both.csv')
 
 #choose the survey indices you wanna use
-surveys <- read.ices("C:/Users/jchampag/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-IO.txt");survey <- 'IO'
+surveys <- read.ices("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-IO.txt");survey <- 'IO'
 surveys <- read.ices("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/data_EDA/surveys-FR-GEAC-OFF.txt");survey <- 'OFF'
 #surveys2 <- read.ices("C:/Users/ChampagnatJ/Dropbox/IFREMER/3Ps_cod/Cours/Cours_Noel_Cadigan/F6005_winter_2019/Project1/surveys-FR-GEAC-OFF.txt")
 
@@ -94,4 +101,4 @@ dat <- setup.sam.data(
 
 
 #saving in a Rdata file
-save(surveys,cn,mo,sw,cw,dw,lw,pf,pm,nm,lf,file=paste("C:/Users/jchampag/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_",survey,".Rdata",sep=""))
+save(surveys,cn,mo,sw,cw,dw,lw,pf,pm,nm,lf,file=paste("C:/Users/ChampagnatJ/Documents/GitHub/SSM3PS/SAM_fit/SAMdata_",survey,".Rdata",sep=""))
